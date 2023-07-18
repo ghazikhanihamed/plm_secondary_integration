3fa7e27f67f18312591148a79c41ac2274f5154a
Hamed Ghazikhani <hamed.ghazikhani@gmail.com>
2023-07-18 17:34:26 -0400

    try ray tune with wandb

--- plm_secondary_integration.py
+++ plm_secondary_integration.py
@@ -3,15 +3,38 @@ from transformers import (
     AutoTokenizer,
     TrainingArguments,
     Trainer,
-    set_seed,
 )
 from datasets import load_dataset
 from transformers.trainer_utils import set_seed
 import logging
 import torch
-import numpy as np
 import sys
 
+from ray import tune
+from ray.air.integrations.wandb import WandbLoggerCallback
+from ray.tune.search.hyperopt import HyperOptSearch
+from ray.tune.schedulers import ASHAScheduler
+from ray.tune import CLIReporter
+
+import wandb
+
+import json
+
+import numpy as np
+
+
+with open('wandb_config.json') as f:
+    data = json.load(f)
+
+api_key = data['wandb']['api_key']
+
+wandb_config = {
+    "wandb": {
+        "project": "Protein-Structure-Prediction",
+    }
+}
+
+wandb.login(key=api_key)
 
 logger = logging.getLogger(__name__)
 
@@ -32,21 +55,16 @@ training_dataset = load_dataset(
     "proteinea/secondary_structure_prediction",
     data_files={"train": ["training_hhblits.csv"]},
 )
-casp12_dataset = load_dataset(
-    "proteinea/secondary_structure_prediction", data_files={"test": ["CASP12.csv"]}
-)
 
-casp14_dataset = load_dataset(
-    "proteinea/secondary_structure_prediction", data_files={"test": ["CASP14.csv"]}
-)
+# we randomly select 500 samples from the training set to use as our validation set
+validation_dataset = training_dataset["train"].train_test_split(
+    test_size=500, seed=42)
 
 input_column_name = "input"
 labels_column_name = "dssp3"
 
 # concatenate all sequences
-all_sequences = list(training_dataset["train"][input_column_name]) + list(
-    casp12_dataset["test"][input_column_name]
-)
+all_sequences = list(training_dataset["train"][input_column_name])
 
 sequence_lengths = [len(seq.split()) for seq in all_sequences]
 max_length = int(np.percentile(sequence_lengths, 95))
@@ -106,16 +124,11 @@ train_dataset = training_dataset.map(
     remove_columns=training_dataset.column_names["train"],
     desc="Running tokenizer on dataset",
 )
-valid_dataset = casp12_dataset.map(
+
+valid_dataset = validation_dataset.map(
     preprocess_data,
     batched=True,
-    remove_columns=casp12_dataset.column_names["test"],
-    desc="Running tokenizer on dataset",
-)
-test_dataset = casp14_dataset.map(
-    preprocess_data,
-    batched=True,
-    remove_columns=casp14_dataset.column_names["test"],
+    remove_columns=validation_dataset.column_names["train"],
     desc="Running tokenizer on dataset",
 )
 
@@ -145,39 +158,105 @@ def compute_metrics(eval_pred):
 
 
 # Prepare the model
-model = T5ForConditionalGeneration.from_pretrained("ElnaggarLab/ankh-large")
+def model_init():
+    return T5ForConditionalGeneration.from_pretrained("ElnaggarLab/ankh-large")
 
-# Prepare training args
-training_args = TrainingArguments(
-    output_dir="./results",
-    learning_rate=3e-4,
-    do_train=True,
-    do_eval=True,
-    evaluation_strategy="epoch",
-    per_device_train_batch_size=2,
-    per_device_eval_batch_size=2,
-    logging_dir="./logs",
-    logging_strategy="epoch",
-    save_strategy="epoch",
-    deepspeed="ds_config.json",
-    load_best_model_at_end=True,
-    metric_for_best_model="q3_accuracy",
-    greater_is_better=True,
-    num_train_epochs=20,
-    fp16=False,
+
+def train_protein_structure_predictor(config, model_init, train_dataset, valid_dataset, tokenizer):
+    training_args = TrainingArguments(
+        output_dir="./results",
+        learning_rate=config["learning_rate"],
+        do_train=True,
+        do_eval=True,
+        evaluation_strategy="steps",
+        logging_dir="./logs",
+        logging_strategy="steps",
+        save_strategy="steps",
+        deepspeed="ds_config.json",
+        load_best_model_at_end=True,
+        metric_for_best_model="eval_q3_accuracy",
+        greater_is_better=True,
+        num_train_epochs=20,
+        fp16=False,
+        gradient_accumulation_steps=64,
+        weight_decay=config["weight_decay"],
+        max_grad_norm=1.0,
+        max_steps=-1,
+        logging_steps=500,
+        save_steps=500,
+        seed=42,
+        run_name="SS-Generation",
+        auto_find_batch_size=True,
+    )
+    trainer = Trainer(
+        model_init=model_init,
+        args=training_args,
+        train_dataset=train_dataset["train"],
+        eval_dataset=valid_dataset["test"],
+        compute_metrics=compute_metrics,
+        tokenizer=tokenizer,
+    )
+
+    train_result = trainer.train()
+
+    eval_result = trainer.evaluate()
+
+    wandb.log({
+        "train_loss": train_result.training_loss,
+        "eval_loss": eval_result.get("eval_loss", np.nan),
+        "q3_accuracy": eval_result.get("eval_q3_accuracy", np.nan),
+        "epoch": train_result.epoch,
+        "learning_rate": config["learning_rate"],
+        "weight_decay": config["weight_decay"],
+    })
+
+    if training_args.load_best_model_at_end:
+        trainer.model.save_pretrained('./best_model')
+
+    return eval_result
+
+
+# define the hyperparameters search space
+config = {
+    "learning_rate": tune.loguniform(1e-6, 1e-2),
+    "weight_decay": tune.loguniform(1e-6, 1e-4),
+}
+
+# configure the resources per trial for the GPUs
+resources_per_trial = {"gpu": 4}
+
+# define the reporter to fetch the important information
+reporter = CLIReporter(
+    parameter_columns=["learning_rate", "weight_decay"],
+    metric_columns=["loss", "q3_accuracy", "training_iteration"],
 )
 
-# Initialize Trainer
-trainer = Trainer(
-    model=model,
-    args=training_args,
-    train_dataset=train_dataset["train"],
-    eval_dataset=valid_dataset["test"],
-    compute_metrics=compute_metrics,
+# run ray.tune
+analysis = tune.run(
+    train_protein_structure_predictor,
+    search_alg=HyperOptSearch(metric="eval_q3_accuracy", mode="max"),
+    scheduler=ASHAScheduler(metric="eval_q3_accuracy", mode="max"),
+    config=config,
+    num_samples=100,
+    resources_per_trial=resources_per_trial,
+    metric="eval_q3_accuracy",
+    mode="max",
+    callbacks=[WandbLoggerCallback(
+        project=wandb_config["wandb"]["project"],
+        api_key=api_key,
+        log_config=True
+    )],
+    progress_reporter=reporter,
+    local_dir="./ray_results",
 )
 
-# Train the model
-trainer.train()
+# get the best trial
+best_trial = analysis.get_best_trial("eval_q3_accuracy", "max", "last")
 
-# Evaluate the model
-trainer.evaluate()
+# print the best trial's parameters
+best_params = best_trial.config
+print(f"Best trial's parameters: {best_params}")
+
+# retrain your model using the best parameters
+train_protein_structure_predictor(
+    best_params, model_init, train_dataset, valid_dataset, tokenizer)
