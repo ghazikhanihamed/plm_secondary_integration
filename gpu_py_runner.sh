800c35c8a1de06714e9a87ec326990588b08bc1f
Hamed Ghazikhani <hamed.ghazikhani@gmail.com>
2023-07-18 19:05:32 -0400

    some little change

--- gpu_py_runner.sh
+++ gpu_py_runner.sh
@@ -3,14 +3,14 @@
 #$ -N plm_sec
 #$ -cwd
 #$ -m bea
-#$ -l m_mem_free=128G,g=3
+#$ -l m_mem_free=128G,g=4
 
 export TMPDIR=~/tmp
 export TRANSFORMERS_CACHE=~/tmp
 
 source ~/python_venv/bin/activate
 
-deepspeed --num_gpus=3 plm_secondary_integration.py 
+deepspeed --num_gpus=4 plm_secondary_integration.py 
 
 deactivate
 
