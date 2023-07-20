203172c83a2a27f54a5ca9b3acd344d5b4821cb3
Hamed Ghazikhani <hamed.ghazikhani@gmail.com>
2023-07-20 13:26:56 -0400

    try offloading the optimizer

--- gpu_py_runner.sh
+++ gpu_py_runner.sh
@@ -3,14 +3,14 @@
 #$ -N plm_sec
 #$ -cwd
 #$ -m bea
-#$ -l m_mem_free=200G,g=6
+#$ -l m_mem_free=200G,g=4
 
 export TMPDIR=~/tmp
 export TRANSFORMERS_CACHE=~/tmp
 
 source ~/venv_secondary/bin/activate
 
-deepspeed --num_gpus=6 plm_secondary_integration.py 
+deepspeed --num_gpus=4 plm_secondary_integration.py 
 
 deactivate
 
