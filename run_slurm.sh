70449941835c385807fa1058cac12481c852860e
Hamed Ghazikhani <hamed.ghazikhani@gmail.com>
2023-08-01 20:41:12 -0400

    change slurm commands

--- run_slurm.sh
+++ run_slurm.sh
@@ -5,12 +5,24 @@
 
 #SBATCH -J plm_sec
 #SBATCH -o _%x%J.out
-#SBATCH --gpus=10gb:1
+#SBATCH --gpus=10gb:2
 
 # SBATCH -n 1 --ntasks-per-core=1
 
-module avail
-nvidia-smi
+export TMPDIR=~/tmp
+export TRANSFORMERS_CACHE=~/tmp
+
+module load anaconda/3.2023.03
+module load cuda/12.0.0
+module load python/3.9.6 
+
+gcc --version
 nvcc --version
+
+source ~/venv_secondary/bin/activate
+
+accelerate launch --multi_gpu plm_secondary_deepspeed.py
+
+deactivate
 
 module purge
