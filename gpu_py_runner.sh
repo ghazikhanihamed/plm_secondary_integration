bf75f9acaf929dbb89464cd578e4f94957558133
Hamed Ghazikhani <hamed.ghazikhani@gmail.com>
2023-09-05 13:37:51 -0400

    gpu

--- gpu_py_runner.sh
+++ gpu_py_runner.sh
@@ -23,7 +23,7 @@ export TRANSFORMERS_CACHE=~/tmp
 # accelerate launch --multi_gpu preliminary_test.py
 # accelerate launch --multi_gpu plm_secondary_accelerate.py
 
-accelerate launch --config_file /home/h_ghazik/.cache/huggingface/accelerate/default_config.yaml --multi_gpu plm_secondary_accelerate.py
+accelerate launch --config_file /home/h_ghazik/.cache/huggingface/accelerate/default_config.yaml --multi_gpu binary_classification_solubility_task.py
 
 
 conda deactivate
