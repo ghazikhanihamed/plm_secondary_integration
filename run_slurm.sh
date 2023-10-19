d86a3dd61bcd6c9a3e6f52d9b4ea740afda4e6f8
Hamed Ghazikhani <hamed.ghazikhani@gmail.com>
2023-10-19 09:20:15 -0400

    try again

--- run_slurm.sh
+++ run_slurm.sh
@@ -1,10 +1,10 @@
 #!/bin/bash -l
 
 #SBATCH --account=h_ghazik
-#SBATCH --mem=128G
+#SBATCH --mem=512G
 #SBATCH -J plm_sec
 #SBATCH -o _%x%J.out
-#SBATCH --gpus=4
+#SBATCH --gpus=8
 #SBATCH -w virya2
 
 export TMPDIR=~/tmp
