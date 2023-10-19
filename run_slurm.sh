d09dfafcbf5a049a621a9793bf6c4e6bed5ec557
Hamed Ghazikhani <hamed.ghazikhani@gmail.com>
2023-10-19 09:19:23 -0400

    try again

--- run_slurm.sh
+++ run_slurm.sh
@@ -13,6 +13,8 @@ export TRANSFORMERS_CACHE=~/tmp
 module load python/3.9.6
 module load anaconda/3.2023.03
 module load cuda/12.1.1
+
+source /usr/local/pkg/anaconda/v3.2023.03/root/etc/profile.d/conda.sh
 conda activate py39
 
 nvidia-smi
@@ -23,7 +25,7 @@ conda deactivate
 module purge
 
 
-# salloc  -A h_ghazik -t 24:00:00 --mem=64G --gpus=1 
+# salloc  -A h_ghazik -t 24:00:00 --mem=64G --gpus=1
 # ~/plm_secondary_integration/ds_config_p2s.json
 # accelerate launch --multi_gpu plm_secondary_accelerate.py
 # --config_file /home/h_ghazik/.cache/huggingface/accelerate/default_config.yaml
