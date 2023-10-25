12c95da0de76eafd631d7d3a8df79a9997aaa5be
Hamed Ghazikhani <hamed.ghazikhani@gmail.com>
2023-10-25 12:14:32 -0400

    adding secondary integration

---
+++ plm_secondary_training.py
@@ -0,0 +1,198 @@
+from transformers import (
+    T5ForConditionalGeneration,
+    AutoTokenizer,
+    TrainingArguments,
+    Trainer,
+)
+from datasets import load_dataset, concatenate_datasets
+import logging
+import torch
+import numpy as np
+import sys
+
+import wandb
+import json
+import random
+
+# Load Weights & Biases Configuration
+with open("wandb_config.json") as f:
+    data = json.load(f)
+
+api_key = data["api_key"]
+
+wandb_config = {
+    "project": "plm_secondary_integration",
+}
+
+wandb.login(key=api_key)
+
+# Setup logging
+logging.basicConfig(
+    format="%(asctime)s - %(levelname)s - %(name)s - %(message)s",
+    datefmt="%m/%d/%Y %H:%M:%S",
+    handlers=[logging.StreamHandler(sys.stdout)],
+)
+
+tokenizer = AutoTokenizer.from_pretrained("ElnaggarLab/ankh-base", use_fast=True)
+
+# Load the datasets
+dataset1 = load_dataset(
+    "proteinea/secondary_structure_prediction",
+    data_files={"train": ["training_hhblits.csv"]},
+)["train"]
+dataset2 = load_dataset(
+    "proteinea/secondary_structure_prediction", data_files={"CASP12": ["CASP12.csv"]}
+)["CASP12"]
+dataset3 = load_dataset(
+    "proteinea/secondary_structure_prediction", data_files={"CASP14": ["CASP14.csv"]}
+)["CASP14"]
+dataset4 = load_dataset(
+    "proteinea/secondary_structure_prediction", data_files={"CB513": ["CB513.csv"]}
+)["CB513"]
+dataset5 = load_dataset(
+    "proteinea/secondary_structure_prediction", data_files={"TS115": ["TS115.csv"]}
+)["TS115"]
+
+# Concatenate all datasets
+all_datasets = concatenate_datasets([dataset1, dataset2, dataset3, dataset4, dataset5])
+
+# Split the concatenated dataset into training and validation sets
+splits = all_datasets.train_test_split(test_size=0.1, seed=42)
+
+train_dataset = splits["train"]
+validation_dataset = splits["test"]
+
+input_column_name = "dssp3"
+labels_column_name = "dssp3"
+
+max_length = 512
+
+# Consider each label as a tag for each token
+unique_tags = set(tag for doc in train_dataset[labels_column_name] for tag in doc)
+
+# add padding tag
+unique_tags.add("<pad>")
+
+
+tag2id = {tag: id for id, tag in enumerate(unique_tags)}
+id2tag = {id: tag for tag, id in tag2id.items()}
+
+
+def mask_secondary_structure(sequences, mask_prob=0.20, mask_token="<mask>"):
+    masked_sequences = []
+    for seq in sequences:
+        masked_seq = []
+        for element in seq:
+            if random.random() < mask_prob:
+                masked_seq.append(mask_token)
+            else:
+                masked_seq.append(element)
+        masked_sequences.append("".join(masked_seq))
+    return masked_sequences
+
+
+# Mask 15% of secondary structure sequences
+train_dataset[input_column_name] = mask_secondary_structure(
+    train_dataset[input_column_name]
+)
+
+
+def preprocess_data(examples):
+    # Original sequences, labels = examples["input"], examples["dssp3"]
+    # Now focusing on secondary structure
+    sequences = examples["dssp3"]
+
+    # remove whitespace and split each sequence into list of secondary structure elements
+    sequences = [list("".join(seq.split())) for seq in sequences]
+
+    # encode sequences
+    inputs = tokenizer(
+        sequences,
+        add_special_tokens=True,
+        padding="max_length",
+        max_length=max_length,
+        truncation=True,
+        is_split_into_words=True,
+        return_tensors="pt",
+    )
+
+    # This part remains same as labels now point to secondary structure elements
+    labels_encoded = [[tag2id[tag] for tag in seq] for seq in sequences]
+
+    # Pad or truncate the labels to match the sequence length
+    labels_encoded = [
+        label[:max_length] + [tag2id["<pad>"]] * (max_length - len(label))
+        for label in labels_encoded
+    ]
+
+    assert len(inputs["input_ids"]) == len(labels_encoded)
+
+    return {
+        "input_ids": inputs["input_ids"],
+        "attention_mask": inputs["attention_mask"],
+        "labels": torch.tensor(labels_encoded, dtype=torch.long),
+    }
+
+
+train_dataset = train_dataset.map(
+    preprocess_data,
+    batched=True,
+    remove_columns=train_dataset.column_names,
+    desc="Running tokenizer on dataset for training",
+)
+
+valid_dataset = validation_dataset.map(
+    preprocess_data,
+    batched=True,
+    remove_columns=validation_dataset.column_names,
+    desc="Running tokenizer on dataset for validation",
+)
+
+
+# Create the model and prepare it
+model = T5ForConditionalGeneration.from_pretrained("ElnaggarLab/ankh-base")
+
+experiment = "secondary_integration"
+
+# Prepare training args
+training_args = TrainingArguments(
+    output_dir=f"./results_{experiment}",
+    num_train_epochs=68,
+    per_device_train_batch_size=8,
+    per_device_eval_batch_size=8,
+    warmup_steps=10000,
+    learning_rate=4e-3,
+    logging_dir=f"./logs_{experiment}",
+    logging_steps=200,
+    do_train=True,
+    do_eval=True,
+    evaluation_strategy="epoch",
+    gradient_accumulation_steps=32,
+    fp16=False,
+    fp16_opt_level="02",
+    seed=42,
+    save_strategy="epoch",
+    remove_unused_columns=False,
+    run_name="secondary_integration",
+    report_to="wandb",
+    hub_token="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",
+    hub_model_id="ghazikhanihamed/secondary_integration",
+)
+
+# Initialize Trainer
+trainer = Trainer(
+    model=model,
+    args=training_args,
+    train_dataset=train_dataset,
+    eval_dataset=valid_dataset,
+    tokenizer=tokenizer,
+)
+
+# Train the model
+trainer.train()
+
+# We save the best model in the folder "best_model"
+trainer.save_model(f"./best_model_{experiment}")
+
+# Push model to hub
+trainer.push_to_hub(commit_message="PLM-Secondary-Integration")
---
+++ run_slurm_sec.sh
@@ -0,0 +1,41 @@
+#!/bin/bash -l
+
+#SBATCH --account=h_ghazik
+#SBATCH --mem=128G
+#SBATCH -J plm_sec
+#SBATCH -o _%x%J.out
+#SBATCH --gpus=6
+#SBATCH -w virya1
+
+export TMPDIR=~/tmp
+export TRANSFORMERS_CACHE=~/tmp
+
+module load python/3.9.6
+module load anaconda/3.2023.03
+module load cuda/12.1.1
+
+source /usr/local/pkg/anaconda/v3.2023.03/root/etc/profile.d/conda.sh
+conda activate py39
+
+nvidia-smi
+
+accelerate launch plm_secondary_training.py
+
+conda deactivate
+module purge
+
+
+# salloc  -A h_ghazik -t 24:00:00 --mem=64G --gpus=1
+# ~/plm_secondary_integration/ds_config_p2s.json
+# accelerate launch --multi_gpu plm_secondary_accelerate.py
+# --config_file /home/h_ghazik/.cache/huggingface/accelerate/default_config.yaml
+
+
+
+# speed
+# module load python/3.9.1/default
+# module load anaconda3/2023.03/default
+# module load cuda/11.8/default
+
+# source /encs/pkg/anaconda3-2023.03/root/etc/profile.d/conda.csh
+# conda activate py3.9
