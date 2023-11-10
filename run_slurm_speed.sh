1e115f107af8deb37bd4849688d393b04ee571be
Hamed Ghazikhani <56132678+ghazikhanihamed@users.noreply.github.com>
2023-11-10 14:23:58 -0500

    fix slurm for speed

--- run_slurm_speed.sh
+++ run_slurm_speed.sh
@@ -6,6 +6,8 @@
 #SBATCH -o _%x%J.out
 #SBATCH --gres=gpu:1
 #SBATCH -w speed-43
+#SBATCH --partition=ep
+
 
 setenv TMPDIR /nfs/speed-scratch/h_ghazik/tmp
 setenv TRANSFORMERS_CACHE /nfs/speed-scratch/h_ghazik/tmp
@@ -13,6 +15,7 @@ setenv TRANSFORMERS_CACHE /nfs/speed-scratch/h_ghazik/tmp
 module load anaconda3/2023.03/default
 module load pytorch/1.10.0/GPU/default
 
+source /encs/pkg/anaconda3-2023.03/root/etc/profile.d/conda.csh
 conda activate py3.9
 
 python plm_secondary_integration.py
