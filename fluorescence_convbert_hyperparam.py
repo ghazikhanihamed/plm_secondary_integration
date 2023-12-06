c0a8646a3330225da2e378710230ef02c799dc2e
Hamed Ghazikhani <hamed.ghazikhani@gmail.com>
2023-12-06 11:50:21 -0500

    fluorescence hyperparam

---
+++ fluorescence_convbert_hyperparam.py
@@ -0,0 +1,212 @@
+import torch
+import os
+
+os.environ["CUBLAS_WORKSPACE_CONFIG"] = ":4096:8"
+os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+import ankh
+import wandb
+import json
+from functools import partial
+from sklearn import metrics
+from torch.utils.data import Dataset, random_split
+from transformers import (
+    Trainer,
+    TrainingArguments,
+    EvalPrediction,
+    set_seed,
+    EarlyStoppingCallback,
+)
+from load_embeddings import load_embeddings_and_labels
+import numpy as np
+from scipy import stats
+
+# Set seed for reproducibility
+seed = 7
+set_seed(seed)
+
+
+def load_wandb_config():
+    # Load Weights & Biases Configuration
+    with open("wandb_config.json") as f:
+        data = json.load(f)
+    return data["api_key"]
+
+
+def setup_wandb(api_key):
+    # Setup Weights & Biases
+    wandb.login(key=api_key)
+
+
+def load_data():
+    return load_embeddings_and_labels("./embeddings/p2s_fluorescence", "fluorescence")
+
+
+def create_datasets(
+    training_sequences,
+    training_labels,
+    validation_sequences,
+    validation_labels,
+    test_sequences,
+    test_labels,
+):
+    class FluorescenceDataset(Dataset):
+        def __init__(self, sequences, labels):
+            self.sequences = sequences
+            self.labels = labels
+
+        def __getitem__(self, idx):
+            embedding = self.sequences[idx]
+            label = self.labels[idx]
+            return {
+                "embed": torch.tensor(embedding),
+                "labels": torch.tensor(label, dtype=torch.float32).unsqueeze(-1),
+            }
+
+        def __len__(self):
+            return len(self.sequences)
+
+    training_dataset = FluorescenceDataset(training_sequences, training_labels)
+    validation_dataset = FluorescenceDataset(validation_sequences, validation_labels)
+    test_dataset = FluorescenceDataset(test_sequences, test_labels)
+
+    return training_dataset, validation_dataset, test_dataset
+
+
+def model_init(embed_dim, training_labels_mean=None):
+    hidden_dim = int(embed_dim / 2)
+    num_hidden_layers = 1
+    nlayers = 1
+    nhead = 4
+    dropout = 0.2
+    conv_kernel_size = 7
+    pooling = "max"
+    downstream_model = ankh.ConvBertForRegression(
+        input_dim=embed_dim,
+        nhead=nhead,
+        hidden_dim=hidden_dim,
+        num_hidden_layers=num_hidden_layers,
+        num_layers=nlayers,
+        kernel_size=conv_kernel_size,
+        dropout=dropout,
+        pooling=pooling,
+        training_labels_mean=training_labels_mean,
+    )
+    return downstream_model
+
+
+def compute_metrics(p: EvalPrediction):
+    return {
+        "spearmanr": stats.spearmanr(p.label_ids, p.predictions).correlation,
+    }
+
+
+def optuna_hp_space(trial):
+    return {
+        # Learning rate: For fine-tuning, a smaller range focused on smaller values is preferred.
+        "learning_rate": trial.suggest_float("learning_rate", 1e-5, 1e-2, log=True),
+        # Weight decay: This range is reasonable for fine-tuning, but still not too high.
+        "weight_decay": trial.suggest_float("weight_decay", 0.0, 0.1, step=0.01),
+        # Warmup ratio: As before, a step is introduced to not have a very granular search in this space.
+        "warmup_ratio": trial.suggest_float("warmup_ratio", 0.0, 0.5, step=0.05),
+        # Gradient accumulation steps: This is a very important hyperparameter to tune. It is used to simulate
+        "gradient_accumulation_steps": trial.suggest_int(
+            "gradient_accumulation_steps", 1, 16
+        ),
+    }
+
+
+def compute_objective(metrics):
+    # This function will compute the objective for optimization during hyperparameter tuning.
+    return metrics["eval_spearmanr"]
+
+
+def main():
+    model_type = "p2s"
+    experiment = f"fluorescence_hyperparam_{model_type}"
+
+    api_key = load_wandb_config()
+    setup_wandb(api_key)
+
+    (
+        train_embeddings,
+        train_labels,
+        validation_embeddings,
+        validation_labels,
+        test_embeddings,
+        test_labels,
+    ) = load_data()
+
+    # Get the mean of the labels to initialize
+    training_labels_mean = np.mean(train_labels)
+
+    training_dataset, validation_dataset, test_dataset = create_datasets(
+        train_embeddings,
+        train_labels,
+        validation_embeddings,
+        validation_labels,
+        test_embeddings,
+        test_labels,
+    )
+
+    model_embed_dim = 768
+
+    training_args = TrainingArguments(
+        output_dir=f"./results_{experiment}",
+        num_train_epochs=5,
+        per_device_train_batch_size=1,
+        per_device_eval_batch_size=1,
+        warmup_steps=1000,
+        learning_rate=1e-03,
+        logging_dir=f"./logs_{experiment}",
+        logging_steps=200,
+        do_train=True,
+        do_eval=True,
+        evaluation_strategy="epoch",
+        gradient_accumulation_steps=16,
+        fp16=False,
+        fp16_opt_level="O2",
+        run_name=experiment,
+        load_best_model_at_end=True,
+        metric_for_best_model="eval_spearmanr",
+        greater_is_better=True,
+        save_strategy="epoch",
+        report_to="wandb",
+    )
+
+    # Initialize Trainer
+    trainer = Trainer(
+        model_init=partial(
+            model_init,
+            embed_dim=model_embed_dim,
+            training_labels_mean=training_labels_mean,
+        ),
+        args=training_args,
+        train_dataset=training_dataset,
+        eval_dataset=validation_dataset,
+        compute_metrics=compute_metrics,
+        callbacks=[
+            EarlyStoppingCallback(
+                early_stopping_patience=3, early_stopping_threshold=0.05
+            )
+        ],
+    )
+
+    best_trials = trainer.hyperparameter_search(
+        direction="maximize",
+        backend="optuna",
+        hp_space=optuna_hp_space,
+        n_trials=20,
+        compute_objective=compute_objective,
+    )
+
+    print("------------------")
+    print(best_trials.hyperparameters)
+    print("------------------")
+
+    # Make predictions and log metrics
+    predictions, labels, metrics_output = trainer.predict(test_dataset)
+    print("Evaluation metrics: ", metrics_output)
+
+
+if __name__ == "__main__":
+    main()
