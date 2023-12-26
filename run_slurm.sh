c7985e309d23700e8f729841dbc2920fee5582b9
Hamed Ghazikhani <hamed.ghazikhani@gmail.com>
2023-12-26 13:26:01 -0500

    correctly classified sequences

--- run_slurm.sh
+++ run_slurm.sh
@@ -2,7 +2,7 @@
 
 #SBATCH --account=h_ghazik
 #SBATCH --mem=64G
-#SBATCH -J att_trns
+#SBATCH -J fil_class
 #SBATCH -o _%x%J.out
 #SBATCH --gpus=1
 
@@ -18,7 +18,7 @@ conda activate py39
 
 nvidia-smi
 
-python transporters_attention.py
+python filter_classified.py
 
 conda deactivate
 module purge
